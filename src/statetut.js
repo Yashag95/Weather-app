b2eb00f25c4da7f63bfce496898f669d8e89e30e
Yash Agrawal <114916630+Yashag95@users.noreply.github.com>
2023-03-02 16:07:01 +0530

    Add files via upload

---
+++ src/statetut.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import React from 'react'
+
+const statetut = () => {
+    const [name, setName] = useState("enter your name here")
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default statetut
